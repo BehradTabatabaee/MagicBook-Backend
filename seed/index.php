73f3d11fd6635123966223fa8f47081a0dc0bac5
Behrad Tabatabaee <behradtabatabaee3@gmail.com>
2024-05-15 07:24:07 +0330

    half-assed

--- seed/index.php
+++ seed/index.php
@@ -22,9 +22,10 @@
     }
 
     // function to query the database, just to make the code a bit cleaner
-    function createQuery(sqlQuery) {
+    function createQuery($sqlQuery) {
+        global $conn;
         // exit if query fails
-        if ($conn->query($sql) != TRUE) {
+        if ($conn->query($sqlQuery) != TRUE) {
             die("Error : " . $conn->error);
         }
     }
@@ -37,18 +38,38 @@
         userPassword VARCHAR(20) NOT NULL,
         email VARCHAR(50),
         picture VARCHAR(2083)
-        )")
+        )");
 
     createQuery("CREATE TABLE Admins (
         adminName VARCHAR(50) PRIMARY KEY,
-        adminPassword VARCHAR(50) NOT NULL,
-        )")
+        adminPassword VARCHAR(50) NOT NULL
+        )");
     
     createQuery("CREATE TABLE Mangas (
         mangaID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         mangaName VARCHAR(50) NOT NULL,
-        manga VARCHAR(50) NOT NULL,
-        )")
+        mangaPicture VARCHAR(2083),
+        mangaVolumes INT UNSIGNED NOT NULL
+        )");
+    
+    createQuery("CREATE TABLE Volumes (
+        volumeID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        mangaID INT,
+        mangaName VARCHAR(50) NOT NULL,
+        volumeNumber INT NOT NULL,
+        volumeURL VARCHAR(2083) NOT NULL,
+        FOREIGN KEY (mangaID) REFERENCES Mangas(mangaID)
+        )");
 
-    echo json_encode("Tables created and data inserted successfully");
+    createQuery("INSERT INTO Admins (adminName, adminPassword) VALUES ('Behrad', 'Tabatabaee')");
+
+    createQuery("INSERT INTO Users (userName, firstName, lastName, userPassword) VALUES ('Behrad', 'Behrad', 'Tabatabaee', 'Behrad')");
+    
+    createQuery("INSERT INTO Mangas (mangaName, mangaVolumes) VALUES ('Vagabond', 1)");
+
+    createQuery("INSERT INTO Volumes (mangaName, mangaID, volumeNumber, volumeURL) 
+    SELECT 'Vagabond', mangaID, 1, 'https://mangareader.storage.iran.liara.space/vol1.pdf' 
+    FROM Mangas WHERE mangaName = 'Vagabond'");
+
+    echo json_encode("Tables created and data added successfully");
 ?>
